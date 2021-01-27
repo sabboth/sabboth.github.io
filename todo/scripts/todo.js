1448913b8bea72f683a135c5951f199be180e02b
Bill Kolodzey <65090311+sabboth@users.noreply.github.com>
2021-01-27 15:17:29 -0600

    Local storage implementation

--- todo/scripts/todo.js
+++ todo/scripts/todo.js
@@ -1,26 +1,112 @@
 // global definitions
 var taskCount = 0;  // counts all items that ever existed (never decreases)
 var tidyCount = 0;  // number of struck items on list
-var initialItems = ['Add a new task', 'Cross something off'];
+var initialItems = ['Add a new task','0','Cross something off','0'];
+var toDoList = [];
+var positionLookUp = [];
+
+// executed script
+if (localStorage.getItem("toDoListStored") === null) {
+  toDoList = JSON.parse(JSON.stringify(localStorage.getItem("toDoListStored")));
+} else {
+  toDoList = JSON.parse(JSON.stringify(initialItems));
+}
+initializeList()
 
 // functions
 function initializeList() {
   var idText;
   var itemText;
+  var isStruck;
   var i;
-  for (i = 0; i < initialItems.length; i++) {
-    taskCount = taskCount + 1;
+  for (i = 0; i < toDoList.length/2; i++) {
     idText = 'item' + taskCount;
-    itemText = initialItems[i];
+    itemText = toDoList[2*i];
+    isStruck = toDoList[2*i + 1];
     var itemDiv = document.createElement('div');
     itemDiv.innerHTML = itemText;
     itemDiv.setAttribute('id',idText)
     itemDiv.setAttribute('class','item')
     itemDiv.setAttribute('onclick','strike(this.id)')
+    if (isStruck == 1) {
+      itemDiv.style.textDecoration = 'line-through';
+    }
     document.getElementById('list0').appendChild(itemDiv)
+    positionLookUp.push(taskCount)
+    taskCount = taskCount + 1;
   }
-  // initializeDate()
+  localStorage.setItem('toDoListStored',JSON.stringify(toDoList))
 }
+
+function addTask() {
+  var itemText = document.getElementById('textInput0');
+  if (itemText.value) {
+    var idText;
+    idText = 'item' + taskCount;
+    var div = document.createElement('div');
+    div.innerHTML = itemText.value;
+    div.setAttribute('id',idText)
+    div.setAttribute('class','item')
+    div.setAttribute('onclick','strike(this.id)')
+    document.getElementById('list0').appendChild(div)
+    document.getElementById('textInput0').value = '';
+    var position = positionLookUp[taskCount - 1] + 1;
+    positionLookUp.push(position)
+    toDoList.push(itemText)
+    toDoList.push('0')
+    taskCount = taskCount + 1;
+  }
+  focusOnText()
+  localStorage.setItem('toDoListStored',JSON.stringify(toDoList))
+}
+
+function strike(id) {
+  var item = document.getElementById(id);
+  var idNumber = id.slice(4);
+  var position = positionLookUp[idNumber];
+  var decor = item.style.textDecoration;
+  if (decor.localeCompare('line-through')==0) {
+    item.style.textDecoration = 'none';
+    toDoList[2*position + 1] = '0';
+    tidyCount = tidyCount - 1;
+  } else {
+    item.style.textDecoration = 'line-through';
+    toDoList[2*position + 1] = '1';
+    tidyCount = tidyCount + 1;
+  }
+  if (tidyCount == 0) {
+    document.getElementById('tidyButton0').style.visibility = 'hidden';
+  }
+  else{
+    document.getElementById('tidyButton0').style.visibility = 'visible';
+  }
+  localStorage.setItem('toDoListStored',JSON.stringify(toDoList))
+}
+
+function removeStruck() {
+  var idText;
+  for (i = 0; i < taskCount; i++) {
+    idText = 'item' + i;
+    var item = document.getElementById(idText);
+    if (item != null) {
+      var decor = item.style.textDecoration;
+      if (decor.localeCompare('line-through')==0) {
+        item.remove();
+        toDoList.splice(2*positionLookUp[i],2);
+        positionLookUp[i] = -1;
+        for (j = i + 1; j < taskCount; j++) {
+          positionLookUp[j] = positionLookUp[j] - 1;
+        }
+      }
+    }
+  }
+  document.getElementById('tidyButton0').style.visibility = 'hidden';
+  tidyCount = 0;
+  focusOnText()
+  localStorage.setItem('toDoListStored',JSON.stringify(toDoList))
+}
+
+function focusOnText() { document.getElementById('textInput0').focus() }
 
 function initializeDate() {
   var listHead = document.getElementById('listHead0');
@@ -45,60 +131,6 @@ function clearText() {
   var val = item.value;
   if (val.localeCompare('New task')==0) {
     item.value = '';
-  }
-}
-
-function removeStruck() {
-  var idText;
-  for (i = 1; i < taskCount + 1; i++) {
-    idText = 'item' + i;
-    var item = document.getElementById(idText);
-    if (item != null) {
-      var decor = item.style.textDecoration;
-      if (decor.localeCompare('line-through')==0) {
-        item.remove();
-      }
-    }
-  }
-  document.getElementById('tidyButton0').style.visibility = 'hidden';
-  tidyCount = 0;
-  focusOnText()
-}
-
-function focusOnText() { document.getElementById('textInput0').focus() }
-
-function addTask() {
-  var itemText = document.getElementById('textInput0');
-  if (itemText.value) {
-    taskCount = taskCount + 1;
-    var idText;
-    idText = 'item' + taskCount;
-    var div = document.createElement('div');
-    div.innerHTML = itemText.value;
-    div.setAttribute('id',idText)
-    div.setAttribute('class','item')
-    div.setAttribute('onclick','strike(this.id)')
-    document.getElementById('list0').appendChild(div)
-    document.getElementById('textInput0').value = '';
-  }
-  focusOnText()
-}
-
-function strike(id) {
-  var item = document.getElementById(id);
-  var decor = item.style.textDecoration;
-  if (decor.localeCompare('line-through')==0) {
-    item.style.textDecoration = 'none';
-    tidyCount = tidyCount - 1;
-  } else {
-    item.style.textDecoration = 'line-through';
-    tidyCount = tidyCount + 1;
-  }
-  if (tidyCount == 0) {
-    document.getElementById('tidyButton0').style.visibility = 'hidden';
-  }
-  else{
-    document.getElementById('tidyButton0').style.visibility = 'visible';
   }
 }
 
